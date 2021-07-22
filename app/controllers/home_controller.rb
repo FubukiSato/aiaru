96f86454b6d67385f8f8510be8266b6b6cfc0a86
FubukiSato <satofubuki0510@gmail.com>
2021-07-23 00:14:16 +0900

    20210723

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def top
+    @work = Work.find_by(id: 5)
   end
 end
--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,6 +4,10 @@ class PostController < ApplicationController
     end
 
     def create
-        redirect_to("home")
+        @work = Work.new(image: params[:image],stname: params[:STName],occupation: params[:occupation],Salary: params[:Salary],OFhours: params[:OFhours],location: params[:location],treatment: params[:treatment],period: params[:period])
+        @work.save
+        image = params[:image]
+        File.binwrite("public/ptj_images/#{@work.image}",image.read)
+        redirect_to("/home")
     end
 end
