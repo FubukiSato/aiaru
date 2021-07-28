96a8897f5ee0689524feb18236145b935aa72ad2
FubukiSato <satofubuki0510@gmail.com>
2021-07-28 23:40:16 +0900

    add_CarrierWave

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,16 +2,21 @@ class PostController < ApplicationController
     before_action :authenticate_user!
 
     def new
-        @work = Work.find_by(user_id: current_user.id)
+        @work = Work.new
+        if Work.find_by(id: current_user.id)
+            redirect_to("/home")
+        end
     end
 
     def create
-        @work = Work.new(name: params[:name],occupation: params[:occupation],wage: params[:wage],hours: params[:hours],location: params[:location],period: params[:period],link: params[:link],note: params[:note],user_id: current_user.id)
-        @work.image = "#{current_user.id}.jpg"
-        @work.save
-        image = params[:image]
-        File.binwrite("public/#{@work.image}",image.read)
-        redirect_to("/home")
+        @user = User.new(work_params)
+        @user.user_id = current_user.id
+        if @user.save
+            render 'top'
+        else
+            render 'new'
+        end
+        
     end
 
     def edit
@@ -42,6 +47,11 @@ class PostController < ApplicationController
         @work.destroy
         redirect_to("/home");
     end
+
+    private
+        def work_params
+            params.require(:work).permit(:image,:name,:occupation,:wage,:hours,:location,:period,:link,:note)
+        end
 
 
 end
