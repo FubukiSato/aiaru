85145a90d4551399002f7dfaa41a12fdee2eeb5e
FubukiSato <satofubuki0510@gmail.com>
2021-07-29 04:18:33 +0900

    add_flash[:notice]

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,7 +3,8 @@ class PostController < ApplicationController
 
     def new
         if Work.find_by(user_id: current_user.id).present?
-            @work = Work.find_by(user_id: current_user.id)
+            flash[:notice] = "申し訳ありません。アルバイトの登録は1アカウントにつき一つまでです。"
+            redirect_to("/home")
         else
             @work = Work.new
         end
@@ -13,6 +14,7 @@ class PostController < ApplicationController
         @work = Work.new(work_params)
         @work.user_id = current_user.id
         if @work.save
+            flash[:notice] = "アルバイトの作成に成功しました"
             redirect_to("/home")
         else
             render 'new'
@@ -21,15 +23,23 @@ class PostController < ApplicationController
     end
 
     def edit
-        @work = Work.find_by(user_id: current_user.id)
+        if Work.find_by(user_id: current_user.id)
+            @work = Work.find_by(user_id: current_user.id)
+        else
+            flash[:notice] = "まだアルバイトを登録していません"
+            redirect_to("/home")
+        end
     end
 
     def update
         @work = Work.find_by(user_id: current_user.id)
         @work.update(work_params)
-        @work.save
-        
+        if @work.save
+        flahs[:notice] = "アルバイトの内容を更新しました"
         redirect_to("/home")
+        else
+            render 'update'
+        end
     end
 
     def deleteconfirm
@@ -38,8 +48,12 @@ class PostController < ApplicationController
 
     def delete
         @work = Work.find_by(user_id: current_user.id)
-        @work.destroy
-        redirect_to("/home");
+        if @work.destroy
+            flash[:notice] = "アルバイトの募集を削除しました"
+            redirect_to("/home");
+        else
+            render 'deleteconfirm'
+        end
     end
 
     private
