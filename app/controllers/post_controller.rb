c8b855dca729c1712b9bb792e72321ab20817ca1
FubukiSato <satofubuki0510@gmail.com>
2021-08-14 23:54:14 +0900

    add_error_messages

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -70,7 +70,7 @@ class PostController < ApplicationController
         redirect_to("/home")
         else
         flash[:alert] = "アルバイト内容の更新に失敗しました"
-        redirect_to("/home")
+        render("edit")
         end
     end
 
