1499beb487a23d021d6d3e695349197c284080cc
FubukiSato <satofubuki0510@gmail.com>
2021-09-11 06:17:28 +0900

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_filter :miniprofiler
 
-  require "rack-mini-profiler"
-
-  private
-  def miniprofiler
-    Rack::MiniProfiler.authorize_request
-  end
+  
 end
--- db/migrate/20210810101346_remove_status_from_works.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromWorks < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :works, :status, :string
-  end
-end
