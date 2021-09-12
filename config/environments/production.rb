96258b71fcfa14a5df828aca1a678f309605e65a
FubukiSato <satofubuki0510@gmail.com>
2021-09-12 19:22:51 +0900

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,13 +125,16 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://aiaru.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'aiaru.herokuapp.com',
+    :domain         => host,
     :authentication => :plain,
   }
 
